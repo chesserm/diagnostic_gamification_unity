2758c64b7417527a245b43f397b046f26c2c9fdf
chesserm <chesserm@umich.edu>
2020-11-01 12:49:08 -0500

    Initial Commit for Unity
    Initial commit for unity project

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Go to Play Scene
+    public void GoToPlay()
+    {
+        SceneManager.LoadScene("Play");
+    }
+
+    // Go to Shop Scene
+    public void GoToShop()
+    {
+        SceneManager.LoadScene("Shop");
+    }
+
+    // Go to Customize Scene
+    public void GoToCustomize()
+    {
+        SceneManager.LoadScene("Customize");
+    }
+
+    // Go to Statistics Scene
+    public void GoToStatistics()
+    {
+        SceneManager.LoadScene("Statistics");
+    }
+}
