cac307e8c71e6869c41a1ff598a628ea7d174e72
kemptona <kemptona@umich.edu>
2020-11-07 16:23:55 -0500

    working api call

--- Assets/Statistics.cs
+++ Assets/Statistics.cs
@@ -1,14 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Net;
+using System;
+using System.IO;
 
 public class Statistics : MonoBehaviour
 {
     GameObject TextObject;
     Text coins_text;
     int coins;
+
+    GameObject APImessage;
+    Text messagetext;
+    string message;
+
     public void ReturnToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
@@ -21,11 +31,29 @@ public class Statistics : MonoBehaviour
         coins = PlayerPrefs.GetInt("NumCoins");
         TextObject = GameObject.FindGameObjectWithTag("coins_disp");
         coins_text = TextObject.GetComponent<Text>();
+
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("https://diagnostic-gamification-api.herokuapp.com/api/test"));
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        string jsonResponse = reader.ReadToEnd();
+        TestMessage info = JsonUtility.FromJson<TestMessage>(jsonResponse);
+
+        UnityEngine.Debug.Log(info.message);
+
+        message = info.message;
+        APImessage = GameObject.FindGameObjectWithTag("test_message");
+        messagetext = APImessage.GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
         coins_text.text = "coins: " + coins.ToString();
+        messagetext.text = message;
     }
+}
+[System.Serializable]
+public class TestMessage
+{
+    public string message;
 }
