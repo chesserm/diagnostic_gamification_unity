f7a8a6cb6c6d63dfc58157b520dfa6d874dfc0c5
chesserm <chesserm@umich.edu>
2020-11-07 20:44:33 -0500

    Added SymptomList page

---
+++ Assets/PlaySymptomList.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlaySymptomList : MonoBehaviour
+{
+    #region EventHandlers
+
+    // Function to return to main page play loop
+    public void BackButtonhandler()
+    {
+        SceneManager.LoadScene("PlayMain");
+    }
+
+    
+    // Funciton to view data for selected symptom to investigate
+    public void SelectedSymptomButtonHandler()
+    {
+        SceneManager.LoadScene("PlaySymptomData");
+    }
+
+
+    #endregion 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
