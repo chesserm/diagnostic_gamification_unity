5fc3f6758b709965cbb72db3e82bb09c9d9e1b77
chesserm <chesserm@umich.edu>
2020-11-07 20:48:36 -0500

    Added Symptom Data page

---
+++ Assets/PlaySymptomData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlaySymptomData : MonoBehaviour
+{
+    #region EventHandlers
+
+    // Function to Continue to Reasoning Page
+    public void BackButtonhandler()
+    {
+        SceneManager.LoadScene("PlayReasoning");
+    }
+
+
+    #endregion 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
