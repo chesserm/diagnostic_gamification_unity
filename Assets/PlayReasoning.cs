c1f1503fb004a36b705910d089e66f76a0a59edc
chesserm <chesserm@umich.edu>
2020-11-07 20:57:07 -0500

    Added Reasoning Page

---
+++ Assets/PlayReasoning.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayReasoning : MonoBehaviour
+{
+
+    #region EventHandlers
+
+    // Function to continue to Main Play Page after reasoning is selected
+    public void ReasoningButtonHandler()
+    {
+        SceneManager.LoadScene("PlayMain");
+    }
+
+
+    #endregion 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
