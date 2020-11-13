48c5b91946dd16759775dcc6de25688bee6769f8
Mahir Taqbeem <mahirtaq@umich.edu>
2020-11-13 18:20:10 -0500

    scripts and modified ui

--- Assets/Customize.cs
+++ Assets/Customize.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Runtime.Serialization;
 
 public class Customize : MonoBehaviour
 {
+    public ShopItemHandler handler;
+
     public void ReturnToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
@@ -14,7 +17,23 @@ public class Customize : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Load in handler from playerprefs
+        if (PlayerPrefs.HasKey("ShopItemHandler"))
+        {
+            string strHandler = PlayerPrefs.GetString("ShopItemHandler");
+            handler = JsonUtility.FromJson<ShopItemHandler>(strHandler);
+        }
+        else
+        {
+            handler = new ShopItemHandler();
+            handler.Head01 = true;
+            handler.Body01 = true;
+            handler.Face01 = true;
+            handler.BodyGrey = true;
+            handler.Legs01 = true;
+            //TODO: Set the equipped default values
 
+        }
     }
 
     // Update is called once per frame
--- Assets/Shop.cs
+++ Assets/Shop.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Runtime.Serialization;
 
 public class Shop : MonoBehaviour
 {
     // Get coins from database
     public int coins = 1000;
     public Text coinsText;
+    public ShopItemHandler handler;
 
     public void addItem(string item)
     {
         // Add item to playerprefs list if we can have one
         coinsText.text = coins.ToString();
+        //handler.PurchaseItem()
     }
 
     public void ReturnToMainMenu()
@@ -24,12 +27,46 @@ public class Shop : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Load in handler from playerprefs
+        if (PlayerPrefs.HasKey("ShopItemHandler"))
+        {
+            string strHandler = PlayerPrefs.GetString("ShopItemHandler");
+            handler = JsonUtility.FromJson<ShopItemHandler>(strHandler);
+        }
+        else
+        {
+            handler = new ShopItemHandler();
+            handler.Head01 = true;
+            handler.Body01 = true;
+            handler.Face01 = true;
+            handler.BodyGrey = true;
+            handler.Legs01 = true;
+            //TODO: Set the equipped default values
+        }
+        // Load in coin amount from playerprefs
+        if (PlayerPrefs.HasKey("NumCoins"))
+        {
+            coins = PlayerPrefs.GetInt("NumCoins");
+        }
+        else
+        {
+            coins = -1;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnDestroy()
+    {
+        // Save coins in playerprefs
+        PlayerPrefs.SetInt("NumCoins", coins);
+
+        // Save handler in playerprefs
+        string strHandler = JsonUtility.ToJson(handler);
+        PlayerPrefs.SetString("ShopItemHandler", strHandler);
     }
 }
