d7a4770fdda3e0e8aad408095bf032e3dd952519
chesserm <chesserm@umich.edu>
2020-11-14 12:40:30 -0500

    Finalized state management and persistent data
    All state management and persistent data should be good to go. Just added some checks to make doubly sure in certain places. Last remaining pieces from a logic perspective are Summary and Data.

--- Assets/PlaySymptomData.cs
+++ Assets/PlaySymptomData.cs
@@ -6,30 +6,79 @@ using HelperNamespace;
 
 public class PlaySymptomData : MonoBehaviour
 {
+
+    // Boolean to check if this is the first viewing of this data
+    private bool isFirstViewOfData = true;
+
     #region EventHandlers
 
     // Function to Continue to Reasoning Page
     public void ContinueButtonHandler()
     {
-        SceneManager.LoadScene("PlayReasoning");
+        // Don't take the user to the reasoning page for dat they've already answered
+        // reasoning questions about
+        if (isFirstViewOfData)
+        {
+            SceneManager.LoadScene("PlayReasoning");
+        }
+        else
+        {
+            SceneManager.LoadScene("PlayMain");
+        }
+        
     }
 
+    #endregion
 
-    #endregion 
+
+    #region HelperFunctions
+
+    // Check to see if the selected symptom data has been viewed before 
+    // by checking if the key exists in the selected reasoning dictionary
+    private void DetermineIfFirstVisit()
+    {
+        // I know this can be done in one line, but it's clearer and less prone to bugs this way
+        if (CaseInformation.UserReasoning.ContainsKey(CaseInformation.SelectedSymptom))
+        {
+            isFirstViewOfData = false;
+        }
+        else
+        {
+            isFirstViewOfData = true;
+        }
+
+        return;
+    }
+
+    #endregion
+
 
     // Start is called before the first frame update
     void Start()
     {
+        // Check to see if the selected symptom data has been viewed before 
+        DetermineIfFirstVisit();
 
-        UnityEngine.Debug.Log("Inside Play Symptom Data");
-        UnityEngine.Debug.Log(CaseInformation.patient.Age);
-        CaseInformation.patient.Age = 99;
-        UnityEngine.Debug.Log(CaseInformation.patient.Age);
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
         
     }
+
+
+
+    void OnDestroy()
+    {
+        // If the user has seen this data before, the event handler will skip reasoning and 
+        // take them back to the main play page. Therefore, we need to reset the SelectedSymptom
+        if (!isFirstViewOfData)
+        {
+            CaseInformation.SelectedSymptom = SymptomState.Nothing;
+        }
+    }
+
 }
