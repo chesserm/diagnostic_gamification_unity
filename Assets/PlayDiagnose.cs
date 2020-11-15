74faae9338893932c123d7cfdc1adc797c022309
chesserm <chesserm@umich.edu>
2020-11-15 15:15:29 -0500

    Award system finalized

--- Assets/PlayDiagnose.cs
+++ Assets/PlayDiagnose.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using HelperNamespace;
 
 public class PlayDiagnose : MonoBehaviour
 {
 
     #region ButtonHandlers
 
-    // Function to return to main menu
+    // Function to return to main play page
     public void BackButtonHandler()
     {
         SceneManager.LoadScene("PlayMain");
@@ -18,18 +19,22 @@ public class PlayDiagnose : MonoBehaviour
     // Function to go to Diagnose page  (with Pneumonia)
     public void CHFButtonHandler()
     {
+        CaseInformation.UserDiagnosis = DiagnosisState.CHF;
         SceneManager.LoadScene("PlaySummary");
     }
 
     // Function to go to Diagnose page  (with Pneumonia)
     public void COPDButtonHandler()
     {
+        CaseInformation.UserDiagnosis = DiagnosisState.COPD;
         SceneManager.LoadScene("PlaySummary");
     }
 
     // Function to go to Diagnose Page (with Pneumonia)
     public void PneumoniaButtonHandler()
     {
+        // Set user diagnosis
+        CaseInformation.UserDiagnosis = DiagnosisState.Pneumonia;
         SceneManager.LoadScene("PlaySummary");
     }
 
@@ -39,7 +44,8 @@ public class PlayDiagnose : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Making sure this variable is reset before the user selects a Diagnosis
+        CaseInformation.UserDiagnosis = DiagnosisState.Undiagnosed;
     }
 
     // Update is called once per frame
--- Assets/PlaySummary.cs
+++ Assets/PlaySummary.cs
@@ -7,13 +7,24 @@ using HelperNamespace;
 
 public class PlaySummary : MonoBehaviour
 {
-    #region ButtonHandlers
 
-    int coins = 777;
-    int exp = 5000;
-    
-    GameObject containerObject;
-    Player player;
+    #region MemberVariables
+
+    // Variables that store the amount awarded to user 
+    private int coinsAwarded;
+    private int expAwarded;
+
+    // Variable that scales awards based on difficulty of case
+    private int difficultyMod;
+
+    // Variable to store whether or not the user diagnosis was correct
+    private bool wasUserCorrect = false;
+
+    #endregion
+
+
+    // Button handler functions
+    #region ButtonHandlers
 
     // Function to return to main menu
     public void ContinueButtonHandler()
@@ -24,22 +35,207 @@ public class PlaySummary : MonoBehaviour
     #endregion
 
 
-    // Start is called before the first frame update
-    void Start()
+
+    // All private functions used only here
+    // Contains subregions for award system and display functions
+    #region HelperFunctions
+
+
+    // All functions that deal with the award system
+    #region AwardSystem
+
+    // Determine the difficulty modifier to be applied to reward amounts
+    private void DetermineDifficultyModifier()
     {
-        containerObject = GameObject.FindGameObjectWithTag("ContainerObject");
-        player = containerObject.GetComponent<Player>();
-
+        string difficulty = CaseInformation.patient.Difficulty;
+        difficulty = difficulty.ToLower();
         
+        if (difficulty == "easy")
+        {
+            difficultyMod = 1;
+        }
+        else if (difficulty == "medium")
+        {
+            difficultyMod = 2;
+        }
+        else if (difficulty == "hard")
+        {
+            difficultyMod = 3;
+        }
+        else
+        {
+            // Handles potential DB error for Difficulty field
+            difficultyMod = 1;
+        }
 
+        return;
+    }
+
+    // Determine award for the user's Diagnostic accuracy
+    private void DetermineDiagnosisReward()
+    {
+        if (wasUserCorrect)
+        {
+            coinsAwarded = 100;
+            expAwarded = 250;
+        }
+        else
+        {
+            // Base rewards
+            coinsAwarded = 50;
+            expAwarded = 100;
+        }
+
+        // Scale based on difficulty
+        coinsAwarded *= difficultyMod;
+        expAwarded *= difficultyMod;
+
+        return;
+
+    }
+
+
+    // Determines how much experience and coins to award user with for
+    // reasoning choices
+    private void DetermineReasoningReward()
+    {
+        // Number of symptoms the user could have investigated (that have reasoning)
+        int maxNumSymptoms = 9;
+
+        // Get size of reasoning dictionary to determine the number of 
+        // symptoms the user chose to investigate
+        int numSymptomsInvestigated = CaseInformation.UserReasoning.Count;
+
+
+        // Determine the number of symptoms where the user selected
+        // the correct reasoning choice
+        int numCorrectReasonings = 0;
+
+        List<SymptomState> reasoningChoices = new List<SymptomState>(CaseInformation.UserReasoning.Keys);
+        foreach (var symptom in reasoningChoices)
+        {
+            if (CaseInformation.UserReasoning[symptom] == ReasoningState.Correct)
+            {
+                numCorrectReasonings += 1;
+            }
+        }
+
+        // Add rewards for correct reasoning
+        // Not impacted by difficulty mod since reasoning choices are independent of difficulty
+        coinsAwarded += (numCorrectReasonings * 10);
+        expAwarded   += (numCorrectReasonings * 25);
+
+
+        // Bonus for needing minimal information to make the diagnosis
+        // 9  * (number divisible by 3 and 2)
+        int coinInfobonus =  (maxNumSymptoms * (60 / difficultyMod)) -  ((60 / difficultyMod) * numSymptomsInvestigated);
+        int expInfoBonus  =  (maxNumSymptoms * (90 / difficultyMod)) -  ((90 / difficultyMod) * numSymptomsInvestigated);
+
+        coinsAwarded += coinInfobonus;
+        expAwarded += expInfoBonus;
+
+        return;
+    }
+
+
+    // Function that actually awards player and updates player prefs
+    private void AwardPlayer()
+    {
+        // Establish reference to Player object 
+        // (stateless class/script connected to an otherwise empty GameObject)
+        GameObject containerObject = GameObject.FindGameObjectWithTag("ContainerObject");
+        Player player = containerObject.GetComponent<Player>();
+
+
+        // Parameter passed into player class works as follows
         // 'c' = COPD
         // 'p' = Pnueomonia
         // 'h' = CHF
-        char casetype = 'c';
-        bool correct = true;
+        char caseType = 'e';
+        switch (CaseInformation.TrueDiagnosis)
+        {
+            case (DiagnosisState.CHF):
+            {
+                caseType = 'h';
+                break;
+            }
+            case (DiagnosisState.COPD):
+            {
+                caseType = 'c';
+                break;
+            }
+            case (DiagnosisState.Pneumonia):
+            {
+                caseType = 'p';
+                break;
+            }
+    
+        }
+        
+        // Use function from player class to update PlayerPrefs based on these values
+        player.CompleteCase(coinsAwarded, expAwarded, caseType, wasUserCorrect);
+        return;
+    }
 
-        player.CompleteCase(coins, exp, casetype, correct);
 
+    #endregion
+
+
+    // All functions used to display information on the screen
+    #region DisplayFunctions
+
+
+    private void DisplayDiagnosis()
+    {
+
+    }
+
+    private void DisplayReasoning()
+    {
+
+    }
+
+
+    private void DisplayExpertComment()
+    {
+
+    }
+
+
+    private void DisplayRewards()
+    {
+
+    }
+
+    #endregion
+
+
+    #endregion
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Determine if user got diagnosis correct
+        wasUserCorrect = CaseInformation.UserDiagnosis == CaseInformation.TrueDiagnosis;
+
+
+        // Assign awards. Do not change order of function calls
+        // DetermineDiagnosisReward() assigns default values for rewards
+        // and depends on DetermineDifficultyModifier()
+        DetermineDifficultyModifier();
+        DetermineDiagnosisReward();
+        DetermineReasoningReward();
+
+        // Actually award player (and update PlayerPrefs)
+        AwardPlayer();
+
+
+        // Display summary results on screen
+        DisplayDiagnosis();
+        DisplayReasoning();
+        DisplayExpertComment();
+        DisplayRewards();
 
     }
 
@@ -48,6 +244,8 @@ public class PlaySummary : MonoBehaviour
     {
         
     }
+
+
 
     void OnDestroy()
     {
